b7281f6aab1df490f917578dd3420a9b853ba335
Ketul Shukla <shukla.ke@northeastern.edu>
2018-05-21 21:33:02 -0400

    KS: Merge Sorted Linked List

--- DataStructuresAndAlgorithms/src/linkedlist/MergeLinkedList.java
+++ DataStructuresAndAlgorithms/src/linkedlist/MergeLinkedList.java
@@ -3,15 +3,43 @@ package linkedlist;
 public class MergeLinkedList extends LinkedList {
 
     public static ListNode merge(ListNode headA, ListNode headB){
-
+        if(headA == null){
+            return headB;
+        }
+        ListNode currentA = headA;
+        ListNode temp = null;
+        while(headB != null){
+            if(currentA.data > headB.data) {
+                temp = headB;
+                headB = headB.next;
+                temp.next = currentA;
+                headA = temp;
+                currentA = headA;
+            }else{
+                while(currentA.next != null && currentA.next.data < headB.data){
+                    currentA = currentA.next;
+                }
+                temp = headB;
+                headB = headB.next;
+                temp.next = currentA.next;
+                currentA.next = temp;
+            }
+        }
         return headA;
     }
 
     public static void main(String[] args){
         MergeLinkedList mergeListA = new MergeLinkedList();
-        mergeListA.add(2);
+        mergeListA.add(6);
+        mergeListA.add(5);
+        mergeListA.add(3);
+        mergeListA.add(1);
 
         MergeLinkedList mergeListB = new MergeLinkedList();
-        mergeListB.add(1);
+        mergeListB.add(7);
+        mergeListB.add(4);
+        mergeListB.add(2);
+
+        mergeListA.printList(merge(mergeListA.head, mergeListB.head));
     }
 }
